f1f4bf861a69e620d25836aedf83f125d2f9cbbb
songjian <ansongjian@163.com>
2018-08-09 17:08:35 +0800

    2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Sass from 'sass-loader'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
